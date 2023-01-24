27669a2786280c01adbf61e9303e5913738bd404
Kyle Harrington <czi@kyleharrington.com>
2023-01-24 11:48:12 -0500

    Accept an extra html blob for displaying in browser

--- src/napari_image_generators/fangblenny.py
+++ src/napari_image_generators/fangblenny.py
@@ -4,12 +4,12 @@ import webbrowser
 # because https://en.wikipedia.org/wiki/Bluestriped_fangblenny
 
 
-def visualize_task_in_browser(task):
+def visualize_task_in_browser(task, html_blob=""):
     """Visualize a dask task in the browser"""
 
     content = task.dask._repr_html_()
 
-    html = f"<html>{content}</html>"
+    html = f"<html>{html_blob}{content}</html>"
 
     with tempfile.NamedTemporaryFile("w", delete=False, suffix=".html") as f:
         url = "file://" + f.name
